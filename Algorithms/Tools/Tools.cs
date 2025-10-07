b3cda1b31f8608f1fcb05e8278ae0e00c8c33c1c
Ciprian D <147750749+kiprinel05@users.noreply.github.com>
2025-10-07 15:39:48 +0300

    First commit
    Invert + Mirror + Rotate 90 clockwise & trigonometric

---
+++ Algorithms/Tools/Tools.cs
@@ -0,0 +1,177 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace Algorithms.Tools
+{
+    public class Tools
+    {
+        #region Copy
+        public static Image<Gray, byte> Copy(Image<Gray, byte> inputImage)
+        {
+            Image<Gray, byte> result = inputImage.Clone();
+            return result;
+        }
+
+        public static Image<Bgr, byte> Copy(Image<Bgr, byte> inputImage)
+        {
+            Image<Bgr, byte> result = inputImage.Clone();
+            return result;
+        }
+        #endregion
+
+        #region Invert
+        public static Image<Gray, byte> Invert(Image<Gray, byte> inputImage)
+        {
+            Image<Gray, byte> result = new Image<Gray, byte>(inputImage.Size);
+
+            for (int y = 0; y < inputImage.Height; ++y)
+            {
+                for (int x = 0; x < inputImage.Width; ++x)
+                {
+                    result.Data[y, x, 0] = (byte)(255 - inputImage.Data[y, x, 0]);
+                }
+            }
+            return result;
+        }
+
+        public static Image<Bgr, byte> Invert(Image<Bgr, byte> inputImage)
+        {
+            Image<Bgr, byte> result = new Image<Bgr, byte>(inputImage.Size);
+
+            for (int y = 0; y < inputImage.Height; ++y)
+            {
+                for (int x = 0; x < inputImage.Width; ++x)
+                {
+                    result.Data[y, x, 0] = (byte)(255 - inputImage.Data[y, x, 0]);
+                    result.Data[y, x, 1] = (byte)(255 - inputImage.Data[y, x, 1]);
+                    result.Data[y, x, 2] = (byte)(255 - inputImage.Data[y, x, 2]);
+                }
+            }
+            return result;
+        }
+        #endregion
+
+        #region Convert color image to grayscale image
+        public static Image<Gray, byte> Convert(Image<Bgr, byte> inputImage)
+        {
+            Image<Gray, byte> result = inputImage.Convert<Gray, byte>();
+            return result;
+        }
+        #endregion
+
+        #region Binar
+
+        public static Image<Gray, byte> Binar(Image<Gray, byte> inputImage, byte threshold)
+        {
+            Image<Gray, byte> result = new Image<Gray, byte>(inputImage.Size);
+            for (int y = 0; y < inputImage.Height; ++y)
+            {
+                for (int x = 0; x < inputImage.Width; ++x)
+                {
+                    result.Data[y, x, 0] = (inputImage.Data[y, x, 0] >= threshold) ? (byte)255 : (byte)0;
+                }
+            }
+            return result;
+        }
+
+        #endregion
+
+
+        #region Mirror
+
+        public static Image<Gray, byte> Mirror(Image<Gray, byte> inputImage)
+        {
+            Image<Gray, byte> result = new Image<Gray, byte>(inputImage.Size);
+            for (int y = 0; y < inputImage.Height; ++y)
+            {
+                for (int x = 0; x < inputImage.Width; ++x)
+                {
+                    result.Data[y, x, 0] = inputImage.Data[y, inputImage.Width - 1 - x, 0];
+                }
+            }
+            return result;
+        }
+
+        public static Image<Bgr, byte> Mirror(Image<Bgr, byte> inputImage)
+        {
+            Image<Bgr, byte> result = new Image<Bgr, byte>(inputImage.Size);
+            for (int y = 0; y < inputImage.Height; ++y)
+            {
+                for (int x = 0; x < inputImage.Width; ++x)
+                {
+                    result.Data[y, x, 0] = inputImage.Data[y, inputImage.Width - 1 - x, 0];
+                    result.Data[y, x, 1] = inputImage.Data[y, inputImage.Width - 1 - x, 1];
+                    result.Data[y, x, 2] = inputImage.Data[y, inputImage.Width - 1 - x, 2];
+                }
+            }
+            return result;
+        }
+
+        #endregion
+
+        #region Rotate 90 degrees clockwise
+
+        public static Image<Gray, byte> Rotate90Clockwise(Image<Gray, byte> inputImage)
+        {
+            Image<Gray, byte> result = new Image<Gray, byte>(inputImage.Height, inputImage.Width);
+            for (int y = 0; y < inputImage.Height; ++y)
+            {
+                for (int x = 0; x < inputImage.Width; ++x)
+                {
+                    result.Data[x, inputImage.Height - 1 - y, 0] = inputImage.Data[y, x, 0];
+                }
+            }
+            return result;
+        }
+
+        public static Image<Bgr, byte> Rotate90Clockwise(Image<Bgr, byte> inputImage)
+        {
+            Image<Bgr, byte> result = new Image<Bgr, byte>(inputImage.Height, inputImage.Width);
+            for (int y = 0; y < inputImage.Height; ++y)
+            {
+                for (int x = 0; x < inputImage.Width; ++x)
+                {
+                    result.Data[x, inputImage.Height - 1 - y, 0] = inputImage.Data[y, x, 0];
+                    result.Data[x, inputImage.Height - 1 - y, 1] = inputImage.Data[y, x, 1];
+                    result.Data[x, inputImage.Height - 1 - y, 2] = inputImage.Data[y, x, 2];
+                }
+            }
+            return result;
+        }
+
+        #endregion
+
+        #region Rotate 90 degrees trigonometric
+
+        public static Image<Gray, byte> Rotate90Trigonometric(Image<Gray, byte> inputImage)
+        {
+            Image<Gray, byte> result = new Image<Gray, byte>(inputImage.Height, inputImage.Width);
+            for (int y = 0; y < inputImage.Height; ++y)
+            {
+                for (int x = 0; x < inputImage.Width; ++x)
+                {
+                    result.Data[inputImage.Width - 1 - x, y, 0] = inputImage.Data[y, x, 0];
+                }
+            }
+            return result;
+        }
+
+        public static Image<Bgr, byte> Rotate90Trigonometric(Image<Bgr, byte> inputImage)
+        {
+            Image<Bgr, byte> result = new Image<Bgr, byte>(inputImage.Height, inputImage.Width);
+            for (int y = 0; y < inputImage.Height; ++y)
+            {
+                for (int x = 0; x < inputImage.Width; ++x)
+                {
+                    result.Data[inputImage.Width - 1 - x, y, 0] = inputImage.Data[y, x, 0];
+                    result.Data[inputImage.Width - 1 - x, y, 1] = inputImage.Data[y, x, 1];
+                    result.Data[inputImage.Width - 1 - x, y, 2] = inputImage.Data[y, x, 2];
+                }
+            }
+            return result;
+        }
+
+
+        #endregion
+    }
+}
